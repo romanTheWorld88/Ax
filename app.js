94e89747f959e57a97803b93c0e0669d0820ee31
RyanRoman <C00412434@louisiana.edu>
2023-09-21 17:07:21 -0500

    dad jokes

---
+++ app.js
@@ -0,0 +1,38 @@
+axios.get("https://swapi.dev/api/people/1/").then((res) => {
+  console.log("RESPONSE: ", res);
+});
+
+const getStarWarsPerson = async (id) => {
+  try {
+    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+    console.log(res);
+  } catch (e) {
+    console.log("Error:", e);
+  }
+};
+
+getStarWarsPerson(5);
+getStarWarsPerson(10);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icaasdnhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "NO jokes available sorry!";
+    console.log("Error: ", e);
+  }
+};
+
+button.addEventListener("click", addNewJoke);
